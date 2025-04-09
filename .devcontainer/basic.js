3ae60ef2bd49a5a71588f8a80bf32ddde1a06792
Swati <142683953+30swati@users.noreply.github.com>
2025-04-09 09:14:40 +0000

    container config

---
+++ .devcontainer/basic.js
@@ -0,0 +1,37 @@
+//Variables in js
+//Var => function-scope (ideal practice to not use more often)
+//Let => block scope
+//Const => store value that don't change
+let name = "Swati"
+const password = 1234
+let passwordSet = true 
+let accountID= 882678
+let bulbOn = true
+let emptyID = ""
+let data = null
+let data1 = undefined
+let data2
+console.table([name, password, passwordSet, accountID])
+
+//finding type of each values
+console.log(typeof name) //string
+console.log(typeof password) //number
+console.log(typeof passwordSet) //boolean
+console.log(typeof accountID) //number
+console.log(typeof emptyID) //string
+console.log(typeof data) //object
+console.log(typeof data1)//undefined
+console.log(typeof data2) //undefined
+
+
+//DATATYPES in JS are :-
+//1.String => "" or ''
+//2.boolean => either true or false
+//3.number 
+//4.undefined
+//5.BigInt
+//6.null
+//7.object 
+//8.Symbol
+
+//Conversion in js
